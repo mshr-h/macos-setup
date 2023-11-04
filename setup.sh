61de71b3df24838f02d5735d74ab8992370c1d4b
Masahiro Hiramori <mhg00g13@gmail.com>
2023-11-04 17:32:24 +0900

    update brew

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
+# brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/mshr/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle
